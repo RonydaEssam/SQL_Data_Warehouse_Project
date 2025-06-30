e3cb641ec3ba83a346ea9a59d77451fbcf76a8d4
RonydaEssam <ronyda988@gmail.com>
2025-06-30 19:06:25 +0300

    Update scripts for building silver layer

--- tests/silver_quality_checks.sql
+++ tests/silver_quality_checks.sql
@@ -64,6 +64,69 @@ SELECT * FROM silver.crm_prd_info
 
 
 -- ** Table: crm_sales_details **
+-- -- Check for spaces in strings
+SELECT * 
+FROM silver.crm_sales_details
+WHERE sls_ord_num != TRIM(sls_ord_num)
+
+SELECT * 
+FROM silver.crm_sales_details
+WHERE sls_ord_num != TRIM(sls_ord_num)
+
+-- Check for invalid dates
+SELECT sls_order_dt
+FROM silver.crm_sales_details
+WHERE sls_order_dt <= 0
+OR LEN(sls_order_dt) != 8
+OR sls_order_dt > 20500101
+OR sls_order_dt < 20000101
+
+SELECT sls_ship_dt
+FROM silver.crm_sales_details
+WHERE sls_ship_dt <= 0
+OR LEN(sls_ship_dt) != 8
+OR sls_ship_dt > 20500101
+OR sls_ship_dt < 20000101
+
+SELECT sls_due_dt
+FROM silver.crm_sales_details
+WHERE sls_due_dt <= 0
+OR LEN(sls_due_dt) != 8
+OR sls_due_dt > 20500101
+OR sls_due_dt < 20000101
+
+SELECT *
+FROM silver.crm_sales_details
+WHERE sls_order_dt > sls_ship_dt 
+OR sls_ship_dt > sls_due_dt
+
+SELECT *
+FROM silver.crm_sales_details
+WHERE sls_sales != sls_quantity * sls_price
+OR sls_sales <= 0 OR sls_price <= 0 OR sls_quantity <= 0
+OR sls_sales IS NULL OR sls_price IS NULL OR sls_quantity IS NULL
+ORDER BY sls_sales, sls_quantity, sls_price
+
+SELECT 
+	CASE
+		WHEN sls_sales IS NULL OR sls_sales <= 0 OR sls_sales != sls_quantity * ABS(sls_price) THEN sls_quantity * ABS(sls_price)
+		ELSE sls_sales
+	END AS sls_sales,
+	sls_quantity,
+	CASE
+		WHEN sls_price IS NULL OR sls_price <= 0 THEN ABS(sls_sales) / NULLIF(sls_quantity, 0)
+		ELSE sls_price
+	END AS sls_price
+FROM silver.crm_sales_details
+WHERE sls_sales != sls_quantity * sls_price
+OR sls_sales <= 0 OR sls_price <= 0 OR sls_quantity <= 0
+OR sls_sales IS NULL OR sls_price IS NULL OR sls_quantity IS NULL
+ORDER BY sls_sales, sls_quantity, sls_price
+
+SELECT * FROM silver.crm_sales_details
 
 
-SELECT * FROM bronze.crm_sales_details WHERE sls_order_dt <=0
+-- ** Table: crm_cust_info **
+
+
+SELECT * FROM bronze.erp_cust_az12
